c530b093139d9af6444b9d1cfe535c899ea49f9e
Daniel Andres <daniel.andres@md.de>
2022-02-07 18:23:27 +0100

    test

--- test-scenario/startup.sh
+++ test-scenario/startup.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 minikube start
+
+# remove helm 2 version
+rm /usr/bin/helm
+
+# install helm 3
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+rm get_helm.sh
+ln -s /usr/local/bin/helm /usr/bin/helm
+
